8293ed18212416516b61dd875e7591f9042a0561
Jakub Wąsala <jakub.wasala@appsilon.com>
2025-12-11 16:46:27 +0100

    Day 10 part 2

--- scripts/day10.py
+++ scripts/day10.py
@@ -1,11 +1,9 @@
-from collections import Counter
 from dataclasses import dataclass
-from functools import cache, reduce
-from operator import concat
-from scipy.optimize import minimize
-import numpy as np
+from functools import cache
 
-from tqdm import tqdm
+import numpy as np
+from scipy.optimize import linprog
+from sklearn.preprocessing import MultiLabelBinarizer
 
 
 @dataclass(frozen=True)
@@ -35,12 +33,13 @@ with open("assets/day10.txt") as file:
         machines.append(Machine(lights, buttons, joltages))
 
 
+# Part 1
 def switch(current_lights: tuple[bool], button: tuple[int]):
     return tuple([not x if i in button else x for i, x in enumerate(current_lights)])
 
 
 @cache
-def min_moves_1(machine: Machine, button_states: tuple[bool]):
+def min_moves(machine: Machine, button_states: tuple[bool]):
     current_lights = tuple(False for _ in machine.lights)
 
     for button_idx, is_enabled in enumerate(button_states):
@@ -57,17 +56,27 @@ def min_moves_1(machine: Machine, button_states: tuple[bool]):
     ]
 
     return min(
-        [1 + min_moves_1(machine, buttons) for buttons in options] + [float("inf")]
+        [1 + min_moves(machine, buttons) for buttons in options] + [float("inf")]
     )
 
 
-outs1 = [
-    min_moves_1(m, tuple([False for _ in range(len(m.buttons))]))
-    for m in tqdm(machines)
-]
-ans1 = sum(outs1)
+ans1 = sum(
+    [min_moves(m, tuple([False for _ in range(len(m.buttons))])) for m in machines]
+)
 
 print("Part 1:", ans1)
 
 
-# Part 2: linear programming problem
+# Part 2
+ans2 = 0
+
+for machine in machines:
+    c = np.ones(len(machine.buttons), dtype=np.int32)
+    b_eq = np.array(machine.joltages)
+    A_eq = MultiLabelBinarizer().fit_transform(machine.buttons).T
+
+    sln = linprog(c=c, A_eq=A_eq, b_eq=b_eq, integrality=np.ones_like(c))
+
+    ans2 += round(sln.fun)
+
+print("Part 2:", ans2)
